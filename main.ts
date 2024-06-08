228dc525d4d3670dd3d384cc95fc2a61719bd536
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 16:48:33 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,29 @@
+ let guestlit :string [] = ["hammad ali","shehbaz","sami"]; 
+
+ let absenGuest = "hammad ali";
+
+ let newGUEST = "ahmed ali";
+
+ guestlit [0] = newGUEST;
+
+ for(let i = 0; i < guestlit.length; i++){
+
+
+ }
+    console.log("Dear Mr/n", guestlit[i] ,"/nIt is our pleasure to Thank you to dinner /n Thank you");
+console.log(absenGuest,"Is not coming to the dinner");
+
+
+console.log("Good news,we found a bigger dinner table so we invite 3 mor guest");
+
+guestlit.unshift("naveed ahmed");
+guestlit.splice(2, 0,"feroz");
+guestlit.push("hammad ali");
+
+
+for (let i = 0; i <guestlit.length; i++);
+    console.log("Dear Mr/n",guestlit[i] ,"/nIt is our pleasure to invite you to dinner /n Yhank you");
+
+{
+
+}
